95f1fa6ae7881c3cb6c2f3709a41d0533c03c132
yannick <yannick.dominguez@gmail.com>
2014-06-29 16:09:17 +0200

    replace tabs by spaces

--- cocktail/ds/FastNode.hx
+++ cocktail/ds/FastNode.hx
@@ -13,10 +13,10 @@ package cocktail.ds;
  * type parameters no longer compile
  */
 interface IFastNode<NodeClass> {
-	var nextSibling:NodeClass;
-	var previousSibling:NodeClass;
-	var parentNode:NodeClass;
-	function removeChild(oldChild:NodeClass):Void;
+    var nextSibling:NodeClass;
+    var previousSibling:NodeClass;
+    var parentNode:NodeClass;
+    function removeChild(oldChild:NodeClass):Void;
 }
 
 /**
@@ -33,119 +33,119 @@ interface IFastNode<NodeClass> {
  */
 class FastNode<NodeClass:IFastNode<NodeClass>> implements IFastNode<NodeClass>
 {
-	public var parentNode:NodeClass;
+    public var parentNode:NodeClass;
 
-	public var firstChild:NodeClass;
-	
-	public var lastChild:NodeClass;
-	
-	public var nextSibling:NodeClass;
-	
-	public var previousSibling:NodeClass;
-	
-	public function new() 
-	{
-		
-	}
-	
-	public function removeChild(oldChild:NodeClass):Void
-	{
-		oldChild.parentNode = null;
-		
-		if (firstChild == oldChild && lastChild == oldChild)
-		{
-			firstChild = null;
-			lastChild = null;
-		}
-		else if (firstChild == oldChild)
-		{
-			var nextSibling = oldChild.nextSibling;
-			firstChild = nextSibling;
-			if (nextSibling != null)
-			{
-				nextSibling.previousSibling = null;
-			}
-		}
-		else if (lastChild == oldChild)
-		{
-			var previousSibling = oldChild.previousSibling;
-			lastChild = previousSibling;
-			if (previousSibling != null)
-			{
-				previousSibling.nextSibling = null;
-			}
-		}
-		else
-		{
-			var previousSibling = oldChild.previousSibling;
-			var nextSibling = oldChild.nextSibling;
-			
-			previousSibling.nextSibling = nextSibling;
-			nextSibling.previousSibling = previousSibling;
-		}
-		
-		oldChild.previousSibling = null;
-		oldChild.nextSibling = null;
-	}
-	
-	public function appendChild(newChild:NodeClass):Void
-	{
-		removeFromParentIfNecessary(newChild);
-		newChild.parentNode = cast(this);
-		
-		if (firstChild == null)
-		{
-			firstChild = newChild;
-			lastChild = newChild;
-			newChild.previousSibling = null;
-			newChild.nextSibling = null;
-		}
-		else
-		{
-			var oldLastChild = lastChild;
-			lastChild = newChild;
-			oldLastChild.nextSibling = newChild;
-			newChild.previousSibling = oldLastChild;
-			newChild.nextSibling = null;
-		}
-	}
-	
-	private function removeFromParentIfNecessary(newChild:NodeClass):Void
-	{
-		if (newChild.parentNode != null)
-		{
-			var parentNode:NodeClass = newChild.parentNode;
-			parentNode.removeChild(newChild);
-		}
-	}
-	
-	public function insertBefore(newChild:NodeClass, refChild:NodeClass):Void
-	{
-		if (firstChild == null || refChild == null)
-		{
-			appendChild(newChild);
-			return;
-		}
-		
-		removeFromParentIfNecessary(newChild);
-		newChild.parentNode = cast(this);
-		
-		var oldPreviousSibling = refChild.previousSibling;
-		if (oldPreviousSibling == null)
-		{
-			firstChild = newChild;
-			refChild.previousSibling = newChild;
-			newChild.nextSibling = refChild;
-			newChild.previousSibling = null;
-		}
-		else
-		{
-			oldPreviousSibling.nextSibling = newChild;
-			refChild.previousSibling = newChild;
-			
-			newChild.previousSibling = oldPreviousSibling;
-			newChild.nextSibling = refChild;
-		}
-	}
-	
+    public var firstChild:NodeClass;
+    
+    public var lastChild:NodeClass;
+    
+    public var nextSibling:NodeClass;
+    
+    public var previousSibling:NodeClass;
+    
+    public function new() 
+    {
+        
+    }
+    
+    public function removeChild(oldChild:NodeClass):Void
+    {
+        oldChild.parentNode = null;
+        
+        if (firstChild == oldChild && lastChild == oldChild)
+        {
+            firstChild = null;
+            lastChild = null;
+        }
+        else if (firstChild == oldChild)
+        {
+            var nextSibling = oldChild.nextSibling;
+            firstChild = nextSibling;
+            if (nextSibling != null)
+            {
+                nextSibling.previousSibling = null;
+            }
+        }
+        else if (lastChild == oldChild)
+        {
+            var previousSibling = oldChild.previousSibling;
+            lastChild = previousSibling;
+            if (previousSibling != null)
+            {
+                previousSibling.nextSibling = null;
+            }
+        }
+        else
+        {
+            var previousSibling = oldChild.previousSibling;
+            var nextSibling = oldChild.nextSibling;
+            
+            previousSibling.nextSibling = nextSibling;
+            nextSibling.previousSibling = previousSibling;
+        }
+        
+        oldChild.previousSibling = null;
+        oldChild.nextSibling = null;
+    }
+    
+    public function appendChild(newChild:NodeClass):Void
+    {
+        removeFromParentIfNecessary(newChild);
+        newChild.parentNode = cast(this);
+        
+        if (firstChild == null)
+        {
+            firstChild = newChild;
+            lastChild = newChild;
+            newChild.previousSibling = null;
+            newChild.nextSibling = null;
+        }
+        else
+        {
+            var oldLastChild = lastChild;
+            lastChild = newChild;
+            oldLastChild.nextSibling = newChild;
+            newChild.previousSibling = oldLastChild;
+            newChild.nextSibling = null;
+        }
+    }
+    
+    private function removeFromParentIfNecessary(newChild:NodeClass):Void
+    {
+        if (newChild.parentNode != null)
+        {
+            var parentNode:NodeClass = newChild.parentNode;
+            parentNode.removeChild(newChild);
+        }
+    }
+    
+    public function insertBefore(newChild:NodeClass, refChild:NodeClass):Void
+    {
+        if (firstChild == null || refChild == null)
+        {
+            appendChild(newChild);
+            return;
+        }
+        
+        removeFromParentIfNecessary(newChild);
+        newChild.parentNode = cast(this);
+        
+        var oldPreviousSibling = refChild.previousSibling;
+        if (oldPreviousSibling == null)
+        {
+            firstChild = newChild;
+            refChild.previousSibling = newChild;
+            newChild.nextSibling = refChild;
+            newChild.previousSibling = null;
+        }
+        else
+        {
+            oldPreviousSibling.nextSibling = newChild;
+            refChild.previousSibling = newChild;
+            
+            newChild.previousSibling = oldPreviousSibling;
+            newChild.nextSibling = refChild;
+        }
+    }
+    
 }
