8cc357e66ba0454ec64d3f02c77d55caf85ef0d1
yannick <yannick.dominguez@gmail.com>
2014-06-29 16:03:12 +0200

    switch to buddy instead of munit

---
+++ test/unit/TestMain.hx
@@ -0,0 +1,13 @@
+package unit;
+
+import buddy.*;
+using buddy.Should;
+
+import cocktail.ds.FastNode;
+
+class TestMain extends BuddySuite implements Buddy {
+
+    public function new()
+    {
+    }
+}
